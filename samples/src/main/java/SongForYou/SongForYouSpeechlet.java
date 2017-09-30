113d0558002cacbafcd89be60129ae85797d1d53
Yulin Xie <xyulin@amazon.com>
2017-09-29 17:02:09 -0700

    enabled contact lookup, tested

--- samples/src/main/java/SongForYou/SongForYouSpeechlet.java
+++ samples/src/main/java/SongForYou/SongForYouSpeechlet.java
@@ -1,12 +1,20 @@
 package SongForYou;
 
+import SongForYou.Contacts.ContactsDao;
 import com.amazon.speech.slu.Intent;
 import com.amazon.speech.speechlet.*;
+import com.amazon.speech.ui.OutputSpeech;
+import com.amazon.speech.ui.PlainTextOutputSpeech;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SongForYouSpeechlet implements Speechlet {
     private static final Logger log = LoggerFactory.getLogger(SongForYouSpeechlet.class);
+    private ContactsDao contactsDao;
+
+    private static final String CONTACT_NAME_SLOT_TYPE = "ContactName";
+    private static final String SONG_NAME_SLOT_TYPE = "SongName";
+    private static final String DEFAULT_PHONENUMBER = "7348461740";
 
     @Override
     public void onSessionStarted(final SessionStartedRequest request, final Session session)
@@ -35,8 +43,29 @@ public class SongForYouSpeechlet implements Speechlet {
         initializeComponents();
 
         Intent intent = request.getIntent();
+
         if ("RequestSongIntent".equals(intent.getName())) {
-            return new SpeechletResponse();
+            // Find the url for this song
+            final String songName = intent.getSlot(SONG_NAME_SLOT_TYPE).getValue();
+
+            // Find the contact
+            final String contactName = intent.getSlot(CONTACT_NAME_SLOT_TYPE).getValue();
+            String phoneNumber = DEFAULT_PHONENUMBER;
+            try {
+                phoneNumber = contactsDao.retrieveContactNumber(contactName);
+            }
+            catch (NullPointerException ex) {
+                log.error("Contact {} is not in the database", contactName);
+            }
+            log.info("Retrieved phone number {} for contact {}", phoneNumber, contactName);
+
+            // Send notification
+
+            // Return response
+
+            PlainTextOutputSpeech outputSpeech = new PlainTextOutputSpeech();
+            outputSpeech.setText("OK, sending song to " + contactName);
+            return SpeechletResponse.newTellResponse(outputSpeech);
         }
         else if ("PlaySongIntent".equals(intent.getName())) {
             return new SpeechletResponse();
@@ -56,6 +85,6 @@ public class SongForYouSpeechlet implements Speechlet {
 
     private void initializeComponents() {
         //Database stuff and other components
-
+        this.contactsDao = new ContactsDao();
     }
 }
