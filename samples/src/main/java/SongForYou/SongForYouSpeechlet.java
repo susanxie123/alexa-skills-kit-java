762a1ffc3895ad9ccb9662fbe8a1d61c27ff38c4
kenelm007 <jessepan@umich.edu>
2017-10-01 23:03:46 -0700

    add function to get song url by name (only 30s preview now/tested separately)

--- samples/src/main/java/SongForYou/SongForYouSpeechlet.java
+++ samples/src/main/java/SongForYou/SongForYouSpeechlet.java
@@ -1,5 +1,8 @@
 package SongForYou;
 
+import java.io.*;
+import java.net.*;
+import org.json.JSONObject;
 import SongForYou.Contacts.ContactsDao;
 import SongForYou.Notifications.NotificationSender;
 import com.amazon.speech.slu.Intent;
@@ -49,6 +52,7 @@ public class SongForYouSpeechlet implements Speechlet {
         if ("RequestSongIntent".equals(intent.getName())) {
             // Find the url for this song
             final String songName = intent.getSlot(SONG_NAME_SLOT_TYPE).getValue();
+            final String songUrl = getSongUrl(songName);
 
             // Find the contact
             final String contactName = intent.getSlot(CONTACT_NAME_SLOT_TYPE).getValue();
@@ -110,5 +114,27 @@ public class SongForYouSpeechlet implements Speechlet {
         //Database stuff and other components
         this.contactsDao = new ContactsDao();
         this.notificationSender = new NotificationSender();
+    }
+
+    public static String getSongUrl(String songName) throws Exception {
+        String query = songName.replace(' ', '+');
+        String url = "https://itunes.apple.com/search?term=" + query + "&limit=1";
+        String result = getHTTP(url);
+        JSONObject jsonObj = new JSONObject(result);
+        return jsonObj.getJSONArray("results").getJSONObject(0).getString("previewUrl");
+    }
+    
+    public static String getHTTP(String urlToRead) throws Exception {
+        StringBuilder result = new StringBuilder();
+        URL url = new URL(urlToRead);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        while ((line = rd.readLine()) != null) {
+           result.append(line);
+        }
+        rd.close();
+        return result.toString();
     }
 }
